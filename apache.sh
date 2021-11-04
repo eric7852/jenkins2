c09fef438f946edadfc910d415a2766d51a36b9c
Seungjun <vdyxz1357@naver.com>
2021-11-04 16:16:43 +0900

    이번엔 제발

--- apache.sh
+++ apache.sh
@@ -5,3 +5,4 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
  systemctl start docker
 docker pull httpd
 docker run -itd -p 60080:80 --name h1 httpd
+echo "byebye"
